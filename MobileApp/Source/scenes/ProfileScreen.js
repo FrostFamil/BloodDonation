11252f294d3a69db03619444cc7e039d33716390
Famil <fsemedli2@gmail.com>
2020-05-01 17:08:55 +0200

    Text files changed and Code verification and Maintainbility added

--- MobileApp/Source/scenes/ProfileScreen.js
+++ MobileApp/Source/scenes/ProfileScreen.js
@@ -1,78 +1,79 @@
-import React, { Component } from "react";
-import {View, Image, Text, TouchableOpacity} from "react-native";
+import React, { Component } from 'react';
+import {
+  View, Image, Text, TouchableOpacity
+} from 'react-native';
 import { CardSection, SettingInput } from '../components';
 
 export default class ProfileScreen extends Component {
-
   constructor(props) {
     super(props);
 
-    this.state = { 
-      fName: 'Jon', 
-      lName: 'Snow', 
-      email: 'jon@snow.com',  
+    this.state = {
+      fName: 'Jon',
+      lName: 'Snow',
+      email: 'jon@snow.com',
       phone: '12345'
     };
   }
 
   render() {
     return (
-      <View style={{ top: 100, justifyContent: 'center', alignItems: 'center'}}>
-        <CardSection style={{ borderRadius: 30}}>
-            <Image
+      <View style={{ top: 100, justifyContent: 'center', alignItems: 'center' }}>
+        <CardSection style={{ borderRadius: 30 }}>
+          <Image
             source={require('../assets/avatar.png')}
             style={styles.ImageIconStyle}
-            />
+          />
         </CardSection>
 
         <CardSection>
           <SettingInput
-          testID="fname"
-          placeholder="First Name"
-          label="First Name:"
-          value={this.state.fName}
-          style={{ height: 40, width: 100 }}
-          onChangeText={text => this.setState({ fName: text})}
-         />
+            testID="fname"
+            placeholder="First Name"
+            label="First Name:"
+            value={this.state.fName}
+            style={{ height: 40, width: 100 }}
+            onChangeText={(text) => this.setState({ fName: text })}
+          />
         </CardSection>
 
         <CardSection>
           <SettingInput
-          testID="lname"
-          placeholder="Last Name"
-          label="Last Name:"
-          value={this.state.lName}
-          style={{ height: 40, width: 100 }}
-          onChangeText={text => this.setState({ lName: text})}
-         />
+            testID="lname"
+            placeholder="Last Name"
+            label="Last Name:"
+            value={this.state.lName}
+            style={{ height: 40, width: 100 }}
+            onChangeText={(text) => this.setState({ lName: text })}
+          />
         </CardSection>
 
-       <CardSection>
-        <SettingInput
-          testID="email"
-          placeholder="user@gmail.com"
-          label="Email:"
-          value={this.state.email}
-          style={{ height: 40, width: 100 }}
-          onChangeText={text => this.setState({ email: text})}
-        />
-       </CardSection>
+        <CardSection>
+          <SettingInput
+            testID="email"
+            placeholder="user@gmail.com"
+            label="Email:"
+            value={this.state.email}
+            style={{ height: 40, width: 100 }}
+            onChangeText={(text) => this.setState({ email: text })}
+          />
+        </CardSection>
 
-       <CardSection>
-        <SettingInput
-           testID="phone"
-           placeholder="+(Country code) 50-465-34-43"
-           label="Phone:"
-           value={this.state.phone}
-           onChangeText={text => this.setState({ phone: text})}
-        />
-       </CardSection>
+        <CardSection>
+          <SettingInput
+            testID="phone"
+            placeholder="+(Country code) 50-465-34-43"
+            label="Phone:"
+            value={this.state.phone}
+            onChangeText={(text) => this.setState({ phone: text })}
+          />
+        </CardSection>
 
-       <View style={styles.ButtonStyle}>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
+        <View style={styles.ButtonStyle}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
             <Text style={{ fontSize: 30 }}>Back</Text>
-        </TouchableOpacity>
-       </View>
+          </TouchableOpacity>
+        </View>
 
       </View>
     );
@@ -90,11 +91,11 @@ const styles = {
     resizeMode: 'stretch',
   },
   ButtonStyle: {
-    top: 20, 
-    backgroundColor: '#f72b2b', 
-    width: 100, 
-    height: 35, 
-    alignItems: 'center', 
+    top: 20,
+    backgroundColor: '#f72b2b',
+    width: 100,
+    height: 35,
+    alignItems: 'center',
     borderRadius: 30
   }
 };
